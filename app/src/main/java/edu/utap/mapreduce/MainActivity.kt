f607f6d36cd26c2585ee27711883b931218e1baa
epines1911 <70340462+epines1911@users.noreply.github.com>
2020-10-30 23:03:36 +0800

    gradle files changed

--- app/src/main/java/edu/utap/mapreduce/MainActivity.kt
+++ app/src/main/java/edu/utap/mapreduce/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.startGameButton
+import kotlinx.android.synthetic.main.end.endBackButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
