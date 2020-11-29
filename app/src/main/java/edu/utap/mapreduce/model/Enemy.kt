25e6e1f92b8232ca85aaa37c1bfe291c1496f9f8
epines1911 <70340462+epines1911@users.noreply.github.com>
2020-11-30 01:08:35 +0800

    Update Enemy.kt
    added enemy.kind

--- app/src/main/java/edu/utap/mapreduce/model/Enemy.kt
+++ app/src/main/java/edu/utap/mapreduce/model/Enemy.kt
@@ -17,38 +17,38 @@ data class Enemy(
     var kind: EnemyKind = EnemyKind.NORMAL
 ) : IAbleToFight
 // assume player(100, 50, 50, 50)
-val testEnemy = Enemy(10, 10, 10, 10, "test enemy name")
-val littleBear = Enemy(60, 30, 10, 50, "Little Bear")
-val rotHound = Enemy(40, 40, 10, 50, "Rot Hound")
-val poppingCandy = Enemy(20, 80, 10, 40, "Popping candy")
-val raven = Enemy(60, 80, 20, 80, "Raven")
-val tramp = Enemy(100, 50, 30, 50, "Tramp")
-val Cerberus = Enemy(60, 60, 20, 80, "Cerberus")
-val hardStone = Enemy(80, 70, 140, 30, "Hard Stone")
-val hedgehog = Enemy(120, 120, 60, 60, "Hedgehog")
+val testEnemy = Enemy(10, 10, 10, 10, "test enemy name", EnemyKind.NORMAL)
+val littleBear = Enemy(60, 30, 10, 50, "Little Bear", EnemyKind.NORMAL)
+val rotHound = Enemy(40, 40, 10, 50, "Rot Hound", EnemyKind.NORMAL)
+val poppingCandy = Enemy(20, 80, 10, 40, "Popping candy", EnemyKind.NORMAL)
+val raven = Enemy(60, 80, 20, 80, "Raven", EnemyKind.NORMAL)
+val tramp = Enemy(100, 50, 30, 50, "Tramp", EnemyKind.NORMAL)
+val Cerberus = Enemy(60, 60, 20, 80, "Cerberus", EnemyKind.NORMAL)
+val hardStone = Enemy(80, 70, 140, 30, "Hard Stone", EnemyKind.NORMAL)
+val hedgehog = Enemy(120, 120, 60, 60, "Hedgehog", EnemyKind.NORMAL)
+val tombstone = Enemy(200, 0, 0, 0, "Tombstone", EnemyKind.NORMAL)
 
-val tombstone = Enemy(200, 0, 0, 0, "Tombstone")
-val berserker = Enemy(50, 180, 280, 200, "Berserker")
-val motherBear = Enemy(300, 160, 50, 60, "Mother bear")
-val wall = Enemy(350, 100, 80, 40, "Wall")
+val berserker = Enemy(50, 180, 280, 200, "Berserker", EnemyKind.ELITE)
+val motherBear = Enemy(300, 160, 50, 60, "Mother bear", EnemyKind.ELITE)
+val wall = Enemy(350, 100, 80, 40, "Wall", EnemyKind.ELITE)
 
 // with toxin
-val Slime = Enemy(160, 70, 0, 60, "Slime")
-val Naga = Enemy(300, 100, 80, 140, "Naga")
+val Slime = Enemy(160, 70, 0, 60, "Slime", EnemyKind.ELITE)
+val Naga = Enemy(300, 100, 80, 140, "Naga", EnemyKind.ELITE)
 
 // If round>4, then nestling will grow up and become loombird
-val nestling = Enemy(150, 50, 0, 300, "Nestling")
-val loomBird = Enemy(320, 130, 20, 150, "Loombird")
+val nestling = Enemy(150, 50, 0, 300, "Nestling", EnemyKind.ELITE)
+val loomBird = Enemy(320, 130, 20, 150, "Loombird", EnemyKind.ELITE)
 
-val hugeHedgehog = Enemy(480, 140, 140, 90, "Huge Hedgehog")
-val terrifiedElephant = Enemy(450, 160, 100, 80, "Terrified elephant")
-val superSlime = Enemy(600, 170, 0, 40, "Super Slime")
-val void = Enemy(520, 200, 90, 130, "Void")
-val Asura = Enemy(1000, 150, 100, 10, "Asura")
+val hugeHedgehog = Enemy(480, 140, 140, 90, "Huge Hedgehog", EnemyKind.ELITE)
+val terrifiedElephant = Enemy(450, 160, 100, 80, "Terrified elephant", EnemyKind.ELITE)
+val superSlime = Enemy(600, 170, 0, 40, "Super Slime", EnemyKind.ELITE)
+val void = Enemy(520, 200, 90, 130, "Void", EnemyKind.ELITE)
+val Asura = Enemy(1000, 150, 100, 10, "Asura", EnemyKind.ELITE)
 
-val bigDevilArm = Enemy(2600, 270, 540, 20, "Big devil's arm")
-val bigDevilTail = Enemy(2200, 600, 270, 460, "Big devil's tail")
-val bigDevilHead = Enemy(1800, 480, 200, 300, "Big devil's head")
+val bigDevilArm = Enemy(2600, 270, 540, 20, "Big devil's arm", EnemyKind.BOSS)
+val bigDevilTail = Enemy(2200, 600, 270, 460, "Big devil's tail", EnemyKind.BOSS)
+val bigDevilHead = Enemy(1800, 480, 200, 300, "Big devil's head", EnemyKind.BOSS)
 
 val AllEnemies = listOf(
     testEnemy, littleBear, rotHound, poppingCandy, raven, tramp,
@@ -69,4 +69,3 @@ val hardEnemies = listOf(
     void,
     Asura
 )
-val bossEnemies = listOf(bigDevilArm, bigDevilTail, bigDevilHead)
